07a6f520e24477746dfe0f1c8cf3de5e4df69b9d
Keith Holliday <keithrholliday@gmail.com>
2018-01-15 16:44:21 -0600

    Changed home file name

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import HomePage from '@/components/HomePage'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HomePage',
-      component: HomePage
+      name: 'Home',
+      component: Home
     },
     {
       path: '/signup',
